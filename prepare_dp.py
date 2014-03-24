95cac2c3e11dfd448d19bc0cd922e2d047dd1cd4
Kacper Wardega <niwatoribaka@gmail.com>
2014-03-24 18:24:15 -0400

    finished no-GUI prepare_dp.py

--- prepare_dp.py
+++ prepare_dp.py
@@ -1,25 +1,30 @@
 import cv2
+import os
 import numpy as np
 from matplotlib import pyplot as plt
-
-MAX_INT = 2 ** 32 - 1
+from gi.repository import GExiv2
 
 def rotate_image(image, angle):
     image_center = tuple(np.array(image.shape) / 2)
-    rot_mat = cv2.getRotationMatrix2D(image_center[:-1], angle, 1.)
-    result = cv2.warpAffine(image, rot_mat, image.shape[:-1], flags=cv2.INTER_LINEAR)
+    if len(image_center) > 2:
+        image_center = image_center[:2]
+    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.)
+    result = cv2.warpAffine(image, rot_mat, image.shape[:2], flags=cv2.INTER_LINEAR)
     return result
 
 def auto_crop(image, sig_brightness):
     img_HSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-    left = MAX_INT
-    right = MAX_INT
-    top = MAX_INT
-    bottom = MAX_INT
+    
+    BIG_NUMBER = 2 ** 32 - 1
+    left = BIG_NUMBER
+    right = BIG_NUMBER
+    top = BIG_NUMBER
+    bottom = BIG_NUMBER
+    
+    check_ratio = 3
 
     for y in range(len(img_HSV)):
-        for x in range(len(img_HSV[y])):
+        for x in range(len(img_HSV[y]))[:len(img_HSV[y]) / check_ratio]:
             if img_HSV[y, x][-1] > sig_brightness:
                 if x < left:
                     left = x
@@ -28,7 +33,7 @@ def auto_crop(image, sig_brightness):
     image = image[:, left:]
     
     for y in range(len(img_HSV)):
-        for x in range(len(img_HSV[y])):
+        for x in range(len(img_HSV[y]))[:len(img_HSV[y]) / check_ratio]:
             if img_HSV[y, len(img_HSV[y]) - 1 - x][-1] > sig_brightness:
                 if x < right:
                     right = x
@@ -37,7 +42,7 @@ def auto_crop(image, sig_brightness):
     img_HSV = img_HSV[:, :right]
     image = image[:, :right]
     
-    for y in range(len(img_HSV)):
+    for y in range(len(img_HSV))[:len(img_HSV) / check_ratio]:
         for x in range(len(img_HSV[y])):
             if img_HSV[y, x][-1] > sig_brightness:
                 if y < top:
@@ -46,7 +51,7 @@ def auto_crop(image, sig_brightness):
     img_HSV = img_HSV[top:, :]
     image = image[top:, :]
     
-    for y in range(len(img_HSV)):
+    for y in range(len(img_HSV))[:len(img_HSV) / check_ratio]:
         for x in range(len(img_HSV[y])):
             if img_HSV[len(img_HSV) - 1 - y, x][-1] > sig_brightness:
                 if y < bottom:
@@ -57,17 +62,61 @@ def auto_crop(image, sig_brightness):
     image = image[:bottom, :]
     return image
 
-def angle_slice(image,angle):
-    image_center = tuple(np.array(image.shape)/2)[:-1]
+def auto_rotate(image, sampling_factor=1.):
+    edges = cv2.Canny(image, 50, 150)
+    small = cv2.resize(edges, (0, 0), fx=0.2, fy=0.2)
+    small = cv2.medianBlur(small, 5)
+    
+    pushed_max = []
+    for i in range(0, int(180 * sampling_factor)):
+        rot_edges = rotate_image(small, i / sampling_factor)
+        pushed = [sum(y) for y in rot_edges]
+        pushed_max.append(max(pushed))
+    
+    pushed_max = smooth(pushed_max, 2)
+    
+    angle = pushed_max.index(max(pushed_max)) / sampling_factor
+    image = rotate_image(image, angle)
+    return image
+    
+def smooth(data, iterations=1, _side=-1):
+    if iterations == 0:
+        return data
+    for i in range(len(data)):
+        if not (i == 0 or i == len(data) - 1):
+            if data[i] > data[i - 1] and data[i] < data[i + 1]:
+                pass
+            elif data[i] < data[i - 1] and data[i] > data[i + 1]:
+                pass
+            else:
+                data[i] = data[i + _side]
+    return smooth(data, iterations - 1, _side * -1)
 
-img = cv2.imread('DPs/raw dp3/photo 1.JPG')
-img = auto_crop(img, 60)
-
-edges = cv2.Canny(img, 400, 100)
-
-# plt.subplot(121),plt.imshow(img,cmap='gray')
-# plt.title('Original'),plt.xticks([]),plt.yticks([])
-plt.subplot(111), plt.imshow(edges, cmap='gray')
-plt.title('Edges'), plt.xticks([]), plt.yticks([])
-
-plt.show()
+if __name__ == '__main__':
+    dir = 'DPs/raw dp3'
+    for fname in sorted(os.listdir(dir)):
+        print '{0} :'.format(fname)
+        
+        path = '{0}/{1}'.format(dir,fname)
+        
+        print '\tClearing EXIF metadata'
+        exif = GExiv2.Metadata(path)
+        exif.clear_exif()
+        exif.clear_xmp()
+        exif.save_file()
+        
+        img = cv2.imread(path)
+        
+        print '\tCropping'
+        img = auto_crop(img, 60)
+        print '\tRotating'
+        img = auto_rotate(img, 2.)
+        
+        cv2.namedWindow('rotated', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('rotated', 800, 600)
+        cv2.startWindowThread()
+        cv2.imshow('rotated', img)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+        # cv2.imwrite('rotated {0}.png'.format(angle), img)
+        
